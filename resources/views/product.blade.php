09af5447232950d583cdcec4da1b9b3003a264df
pameMontreedet <montreedet@3dsinteractive.com>
2018-11-19 18:02:38 +0700

    first commit

---
+++ resources/views/product.blade.php
@@ -0,0 +1,575 @@
+@extends('welcome')
+@section('content')
+<button type="button" class="btn btn-primary">Add</button>
+<div class="table-responsive">
+<table id="dtOrderExample" class="table table-hover table-striped table-bordered table-sm sortable" cellspacing="0" width="100%">
+  <thead>
+    <tr>
+      <th class="th-sm">ID
+
+      </th>
+      <th class="th-sm">NAME
+
+      </th>
+      <th class="th-sm">IMG
+
+      </th>
+      <th class="th-sm">CREATE DATE
+
+      </th>
+      <th class="th-sm">UPDATE DATE
+
+      </th>
+      <th class="th-sm">EDIT
+
+      </th>
+      <th class="th-sm">DELETE
+
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Edinburgh</td>
+      <td>61</td>
+      <td>2011/04/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Garrett Winters</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>63</td>
+      <td>2011/07/25</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Ashton Cox</td>
+      <td>Junior Technical Author</td>
+      <td>San Francisco</td>
+      <td>66</td>
+      <td>2009/01/12</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Cedric Kelly</td>
+      <td>Senior Javascript Developer</td>
+      <td>Edinburgh</td>
+      <td>22</td>
+      <td>2012/03/29</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Airi Satou</td>
+      <td>Accountant</td>
+      <td>Tokyo</td>
+      <td>33</td>
+      <td>2008/11/28</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Brielle Williamson</td>
+      <td>Integration Specialist</td>
+      <td>New York</td>
+      <td>61</td>
+      <td>2012/12/02</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Herrod Chandler</td>
+      <td>Sales Assistant</td>
+      <td>San Francisco</td>
+      <td>59</td>
+      <td>2012/08/06</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Rhona Davidson</td>
+      <td>Integration Specialist</td>
+      <td>Tokyo</td>
+      <td>55</td>
+      <td>2010/10/14</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Colleen Hurst</td>
+      <td>Javascript Developer</td>
+      <td>San Francisco</td>
+      <td>39</td>
+      <td>2009/09/15</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    <tr>
+      <td>Sonya Frost</td>
+      <td>Software Engineer</td>
+      <td>Edinburgh</td>
+      <td>23</td>
+      <td>2008/12/13</td>
+      <td><button type="button" class="btn btn-success">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+@endsection
